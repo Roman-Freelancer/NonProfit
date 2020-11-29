514130d87b4a907a8a53cd7762ba13193afc4e9a
Roman-Freelancer <70280915+Roman-Freelancer@users.noreply.github.com>
2020-11-29 22:53:52 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+//Проверяем на наличие формата WEBP==================================================================
+// function testWebP(callback) {
+//   let webP = new Image();
+//   webP.onload = webP.onerror = function () {
+//     callback(webP.height == 2);
+//   };
+//   webP.src =
+//     "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+// }
+// testWebP(function (support) {
+//   if (support == true) {
+//     document.querySelector("body").classList.add("webp");
+//   } else {
+//     document.querySelector("body").classList.add("no-webp");
+//   }
+// });
+// ===========================================================================================
+$(function () {
+  AOS.init({
+    disable: 'mobile',
+    once: true
+  });
+  $('[data-fancybox="images"]').fancybox({});
+});
